18ca16b485f1ec1cde38ec012fac4f9ccd395e5a
adriarce10 <adriarce10@yahoo.com>
2018-03-15 14:16:41 -0300

    Funcionando

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var express = require('express')
+
+var app = express()
+
+app.listen(3000, () => {
+	console.log('API REST CORRIENDO');
+})
+//var express = require('express');
+//var app = express();
+
+//app.get('/', function (req, res) {
+  //res.send('Hello World!');
+//});
+
+//app.listen(3000, function () {
+  //console.log('Example app listening on port 3000!');
+
+
